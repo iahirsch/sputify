a454c6b29bc6164db154cd6d360cf12cfdc92596
Lukas Bucher <lukas.bucherr@gmail.com>
2024-10-03 23:30:57 +0200

    play in browser working

--- SpotifyAPI/callback.js
+++ SpotifyAPI/callback.js
@@ -2,6 +2,8 @@ const urlParams = new URLSearchParams(window.location.search);
 console.log(urlParams)
 let code = urlParams.get('code');
 
+let playerReady = false;
+
 const getToken = async code => {
 
     // stored in the previous step
@@ -29,6 +31,69 @@ const getToken = async code => {
     console.log(response.access_token);
 }
 
+// Get the wrapped playlists for the years 2019-2023
+const getWrappedPlaylists = async () => {
+    const token = localStorage.getItem('access_token');
+    const playlistIds = [
+        { id: '37i9dQZF1Fa1IIVtEpGUcU', year: 2023 },
+        { id: '37i9dQZF1F0sijgNaJdgit', year: 2022 },
+        { id: '37i9dQZF1EUMDoJuT8yJsl', year: 2021 },
+        { id: '37i9dQZF1EM8zP5iEUpvtS', year: 2020 },
+        { id: '37i9dQZF1EtavyuTuNdfaO', year: 2019 }
+       ];
+
+    const getPlaylist = async (id) => {
+        const url = `https://api.spotify.com/v1/playlists/${id}`;
+        const payload = {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        };
+        const response = await fetch(url, payload);
+        if (!response.ok) {
+            console.warn(`Playlist with ID ${id} does not exist.`);
+            return null;
+        }
+        return response.json();
+    };
+
+    const playlists = await Promise.all(playlistIds.map(({ id }) => getPlaylist(id)));
+    console.log(playlists);
+
+    const playlistContainer = document.getElementById('playlistContainer');
+    playlists.forEach((playlist, index) => {
+        if (playlist) {
+            const button = document.createElement('button');
+            button.textContent = playlistIds[index].year;
+            button.addEventListener('click', () => {
+                const device_id = localStorage.getItem('device_id');
+                const songUri = playlist.tracks.items[0].track.uri;
+                playInBrowser(device_id, songUri);
+            });
+            playlistContainer.appendChild(button);
+        }
+    });
+};
+
+const getTopTracks = async (timeRange) => {
+    const token = localStorage.getItem('access_token');
+    const url = `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}`;
+    const payload = {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    const body = await fetch(url, payload);
+    const response = await body.json();
+    console.log(response);
+
+    // Play the top song
+    const device_id = localStorage.getItem('device_id');
+    const songUri = response.items[0].uri;
+    playInBrowser(device_id, songUri);
+};
+
+//not needed
 const getTopArtists = async () => {
     const token = localStorage.getItem('access_token');
     const url = "https://api.spotify.com/v1/me/top/artists";
@@ -41,7 +106,7 @@ const getTopArtists = async () => {
     const response = await body.json();
     console.log(response);
 }
-
+//not needed
 const getCurrentlyPlaying = async () => {
     const token = localStorage.getItem('access_token');
     const url = "https://api.spotify.com/v1/me/player/currently-playing";
@@ -54,46 +119,7 @@ const getCurrentlyPlaying = async () => {
     const response = await body.json();
     console.log(response);
 }
-
-const getAllPlaylists = async () => {
-    const token = localStorage.getItem('access_token');
-    const url = "https://api.spotify.com/v1/me/playlists";
-    const payload = {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    }
-    const body = await fetch(url, payload);
-    const response = await body.json();
-    console.log(response);
-}
-
-// Get the wrapped playlists for the years 2019-2023
-const getWrappedPlaylists = async () => {
-    const token = localStorage.getItem('access_token');
-    const playlistIds = [
-        '37i9dQZF1Fa1IIVtEpGUcU', //2023
-        '37i9dQZF1F0sijgNaJdgit', //2022
-        '37i9dQZF1EUMDoJuT8yJsl', //2021
-        '37i9dQZF1EM8zP5iEUpvtS', //2020
-        '37i9dQZF1EtavyuTuNdfaO' //2019
-    ];
-
-    const getPlaylist = async (id) => {
-        const url = `https://api.spotify.com/v1/playlists/${id}`;
-        const payload = {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        };
-        const response = await fetch(url, payload);
-        return response.json();
-    };
-
-    const playlists = await Promise.all(playlistIds.map(id => getPlaylist(id)));
-    console.log(playlists);
-};
-
+//not needed
 const playPauseMusic = async () => {
     const token = localStorage.getItem('access_token');
     const url = "https://api.spotify.com/v1/me/player";
@@ -117,7 +143,7 @@ const playPauseMusic = async () => {
         const action = isPlaying ? 'pause' : 'play';
 
         // Make the request to play or pause
-        const actionResponse = await fetch(`${url}/${action}/?device_id=${device_id}`, {
+        const actionResponse = await fetch(`${url}/${action}`, {
             method: 'PUT',
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -134,7 +160,7 @@ const playPauseMusic = async () => {
         console.error('Error in playPauseMusic:', error);
     }
 };
-
+//not needed
 const addSongToQueueAndSkip = async () => {
     const token = localStorage.getItem('access_token');
     const songUri = "spotify:track:351KkakA2YtGEXqSEIIasy";
@@ -171,27 +197,88 @@ const addSongToQueueAndSkip = async () => {
     }
 };
 
-const getTopTracks = async (timeRange) => {
-    const token = localStorage.getItem('access_token');
-    const url = `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}`;
-    const payload = {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    };
-    const body = await fetch(url, payload);
-    const response = await body.json();
-    console.log(response);
-};
-
 document.querySelector('#topTracksShortTerm').addEventListener('click', () => getTopTracks('short_term'));
 document.querySelector('#topTracksMediumTerm').addEventListener('click', () => getTopTracks('medium_term'));
 document.querySelector('#topTracksLongTerm').addEventListener('click', () => getTopTracks('long_term'));
-
 document.querySelector('#token').addEventListener('click', () => getToken(code));
-document.querySelector('#artists').addEventListener('click', () => getTopArtists());
-document.querySelector('#currentlyPlaying').addEventListener('click', () => getCurrentlyPlaying());
-document.querySelector('#allPlaylists').addEventListener('click', () => getAllPlaylists());
+//document.querySelector('#artists').addEventListener('click', () => getTopArtists());
+//document.querySelector('#currentlyPlaying').addEventListener('click', () => getCurrentlyPlaying());
 document.querySelector('#wrappedPlaylists').addEventListener('click', () => getWrappedPlaylists());
-document.querySelector('#playPause').addEventListener('click', () => playPauseMusic());
-document.querySelector('#addSongToQueueAndSkip').addEventListener('click', () => addSongToQueueAndSkip());
+//document.querySelector('#playPause').addEventListener('click', () => playPauseMusic());
+//document.querySelector('#addSongToQueueAndSkip').addEventListener('click', () => addSongToQueueAndSkip());
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = localStorage.getItem('access_token');
+    const player = new Spotify.Player({
+        name: 'Spütify',
+        getOAuthToken: cb => { cb(token); },
+        volume: 0.5
+    });
+
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+        playerReady = true;
+        localStorage.setItem('device_id', device_id);
+        console.log('Ready with Device ID', device_id);
+        //playInBrowser(device_id, songUri);
+        //transferPlayback(device_id);
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+        playerReady = false;
+        console.log('Device ID has gone offline', device_id);
+    });
+
+    player.addListener('initialization_error', ({ message }) => {
+        console.error(message);
+    });
+
+    player.addListener('authentication_error', ({ message }) => {
+        console.error(message);
+    });
+
+    player.addListener('account_error', ({ message }) => {
+        console.error(message);
+    });
+
+    document.getElementById('togglePlay').onclick = function () {
+        player.togglePlay();
+    };
+
+    player.connect();
+}
+
+function transferPlayback(device_id) {
+    const token = localStorage.getItem('access_token');
+    fetch('https://api.spotify.com/v1/me/player', {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "device_ids": [device_id],
+            "play": true // Setze auf true, um die Wiedergabe sofort zu starten
+        })
+    });
+}
+
+function playInBrowser(device_id, songUri) {
+    const token = localStorage.getItem('access_token');
+    if (playerReady) {
+        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "uris": [songUri],
+                play: true
+            })
+        });
+    } else {
+        console.warn('Player not ready. Cannot play music.');
+    }
+}
