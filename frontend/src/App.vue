13372c4f159af20dce242baad296b825562c3077
Lukas Bucher <lukas.bucherr@gmail.com>
2024-10-25 16:19:27 +0200

    adjust to file structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import PrintComponent from './components/PrintComponent.vue';
+import ScrollExample from './components/ScrollExample.vue';
 import ServerTest from './components/ServerTest.vue';
 </script>
 
 <template>
+  <!-- <PrintComponent /> -->
+  <!-- <ScrollExample /> -->
   <ServerTest />
 </template>
