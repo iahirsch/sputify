97e8c86779ffc1130640a213917ea3d39aad3aab
Lukas Bucher <lukas.bucherr@gmail.com>
2024-11-14 15:08:15 +0100

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -109,32 +109,6 @@ app.get('/api/spotify/refresh_token', async function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(data));
 
-    /*
-        var refresh_token = req.query.refresh_token;
-        var authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-            },
-            form: {
-                grant_type: 'refresh_token',
-                refresh_token: refresh_token
-            },
-            json: true
-        };
-    
-        request.post(authOptions, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var access_token = body.access_token,
-                    refresh_token = body.refresh_token;
-                res.send({
-                    'access_token': access_token,
-                    'refresh_token': refresh_token
-                });
-            }
-        });
-        */
 });
 
 
