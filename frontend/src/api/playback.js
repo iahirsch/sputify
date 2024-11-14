9c205939416d1d89e8b2b8cc99a5e39639759d9f
W00dPeckker <marin.hirschi@stud.hslu.ch>
2024-11-14 14:54:01 +0100

    fix cookies, accessToken, separated api calls

---
+++ frontend/src/api/playback.js
@@ -0,0 +1,19 @@
+export async function playback(device_id, songUris, playerReady) {
+    const accessToken = localStorage.getItem('access_token');
+
+    if (playerReady) {
+        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "uris": songUris,
+                play: true
+            })
+        });
+    } else {
+        console.warn('Player not ready. Cannot play music.');
+    }
+}
