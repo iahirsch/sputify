f9cf2b70cc626f662898091ffbbb143226f79aff
Lukas Bucher <lukas.bucherr@gmail.com>
2024-11-14 15:18:50 +0100

    fix imports

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,4 @@
 <script setup>
-import PrintComponent from './components/PrintComponent.vue';
-import ServerTest from './components/ServerTest.vue';
-import SmoothScroll from './components/SmoothScroll.vue';
 </script>
 
 <template>
