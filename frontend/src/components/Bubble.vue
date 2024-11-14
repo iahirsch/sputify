85c8d9683916fa7b9e6cc3b1514bb3b56b9f9c07
Lukas Bucher <lukas.bucherr@gmail.com>
2024-11-14 10:39:54 +0100

    bubble pulse animation

--- frontend/src/components/Bubble.vue
+++ frontend/src/components/Bubble.vue
@@ -17,156 +17,205 @@ export default {
             default: () => [
                 {
                     "start": 0,
-                    "duration": 8.63379,
+                    "duration": 13.32793,
                     "confidence": 1,
-                    "loudness": -12.305,
-                    "tempo": 173.404,
-                    "tempo_confidence": 0.178,
-                    "key": 5,
-                    "key_confidence": 0,
+                    "loudness": -24.081,
+                    "tempo": 132.877,
+                    "tempo_confidence": 0.341,
+                    "key": 0,
+                    "key_confidence": 0.175,
                     "mode": 1,
-                    "mode_confidence": 0,
+                    "mode_confidence": 0.364,
                     "time_signature": 4,
                     "time_signature_confidence": 1
                 },
                 {
-                    "start": 8.63379,
-                    "duration": 26.47246,
+                    "start": 13.32793,
+                    "duration": 26.76783,
                     "confidence": 1,
-                    "loudness": -4.044,
-                    "tempo": 170.076,
-                    "tempo_confidence": 0.607,
-                    "key": 4,
-                    "key_confidence": 0.312,
+                    "loudness": -4.295,
+                    "tempo": 132.225,
+                    "tempo_confidence": 0.472,
+                    "key": 11,
+                    "key_confidence": 0.383,
                     "mode": 0,
-                    "mode_confidence": 0.324,
+                    "mode_confidence": 0.451,
                     "time_signature": 4,
-                    "time_signature_confidence": 1
+                    "time_signature_confidence": 0.926
                 },
                 {
-                    "start": 35.10625,
-                    "duration": 22.2336,
-                    "confidence": 0.779,
-                    "loudness": -5.93,
-                    "tempo": 169.997,
-                    "tempo_confidence": 0.5,
+                    "start": 40.09576,
+                    "duration": 15.33119,
+                    "confidence": 0.406,
+                    "loudness": -5.335,
+                    "tempo": 133.171,
+                    "tempo_confidence": 0.489,
                     "key": 0,
-                    "key_confidence": 0.69,
+                    "key_confidence": 0.882,
                     "mode": 1,
-                    "mode_confidence": 0.785,
+                    "mode_confidence": 0.736,
                     "time_signature": 4,
                     "time_signature_confidence": 1
                 },
                 {
-                    "start": 57.33985,
-                    "duration": 12.35225,
-                    "confidence": 0.586,
-                    "loudness": -3.088,
-                    "tempo": 170.23,
-                    "tempo_confidence": 0.662,
-                    "key": 0,
-                    "key_confidence": 0.793,
-                    "mode": 1,
-                    "mode_confidence": 0.714,
+                    "start": 55.42695,
+                    "duration": 12.63745,
+                    "confidence": 0.111,
+                    "loudness": -3.009,
+                    "tempo": 132.882,
+                    "tempo_confidence": 0.453,
+                    "key": 11,
+                    "key_confidence": 0.188,
+                    "mode": 0,
+                    "mode_confidence": 0.433,
                     "time_signature": 4,
                     "time_signature_confidence": 1
                 },
                 {
-                    "start": 69.6921,
-                    "duration": 17.29427,
-                    "confidence": 0.153,
-                    "loudness": -3.629,
-                    "tempo": 169.952,
-                    "tempo_confidence": 0.567,
+                    "start": 68.0644,
+                    "duration": 15.78345,
+                    "confidence": 0.065,
+                    "loudness": -3.472,
+                    "tempo": 200.023,
+                    "tempo_confidence": 0.499,
+                    "key": 11,
+                    "key_confidence": 0.342,
+                    "mode": 0,
+                    "mode_confidence": 0.511,
+                    "time_signature": 4,
+                    "time_signature_confidence": 0.625
+                },
+                {
+                    "start": 83.84785,
+                    "duration": 15.28977,
+                    "confidence": 0.377,
+                    "loudness": -1.586,
+                    "tempo": 200.528,
+                    "tempo_confidence": 0.365,
+                    "key": 9,
+                    "key_confidence": 0.737,
+                    "mode": 0,
+                    "mode_confidence": 0.479,
+                    "time_signature": 4,
+                    "time_signature_confidence": 1
+                },
+                {
+                    "start": 99.13761,
+                    "duration": 23.69486,
+                    "confidence": 0.311,
+                    "loudness": -3.568,
+                    "tempo": 200.332,
+                    "tempo_confidence": 0.444,
+                    "key": 2,
+                    "key_confidence": 0.403,
+                    "mode": 1,
+                    "mode_confidence": 0.238,
+                    "time_signature": 4,
+                    "time_signature_confidence": 1
+                },
+                {
+                    "start": 122.83247,
+                    "duration": 9.31351,
+                    "confidence": 0.461,
+                    "loudness": -5.003,
+                    "tempo": 200.379,
+                    "tempo_confidence": 0.416,
+                    "key": 11,
+                    "key_confidence": 0.428,
+                    "mode": 0,
+                    "mode_confidence": 0.446,
+                    "time_signature": 4,
+                    "time_signature_confidence": 1
+                },
+                {
+                    "start": 132.14598,
+                    "duration": 29.76011,
+                    "confidence": 0.694,
+                    "loudness": -2.601,
+                    "tempo": 198.094,
+                    "tempo_confidence": 0.295,
                     "key": 7,
-                    "key_confidence": 0.107,
-                    "mode": 1,
-                    "mode_confidence": 0.565,
-                    "time_signature": 4,
-                    "time_signature_confidence": 1
-                },
-                {
-                    "start": 86.98636,
-                    "duration": 26.48214,
-                    "confidence": 0.099,
-                    "loudness": -3.048,
-                    "tempo": 169.674,
-                    "tempo_confidence": 0.486,
-                    "key": 0,
                     "key_confidence": 0,
                     "mode": 1,
-                    "mode_confidence": 0.122,
-                    "time_signature": 4,
-                    "time_signature_confidence": 1
+                    "mode_confidence": 0.239,
+                    "time_signature": 3,
+                    "time_signature_confidence": 0.577
                 },
                 {
-                    "start": 113.4685,
-                    "duration": 27.50251,
-                    "confidence": 0.782,
-                    "loudness": -3.804,
-                    "tempo": 170.033,
-                    "tempo_confidence": 0.543,
-                    "key": 0,
-                    "key_confidence": 1,
+                    "start": 161.9061,
+                    "duration": 11.03014,
+                    "confidence": 0.892,
+                    "loudness": -3.535,
+                    "tempo": 199.497,
+                    "tempo_confidence": 0.259,
+                    "key": 4,
+                    "key_confidence": 0.501,
                     "mode": 1,
-                    "mode_confidence": 0.808,
-                    "time_signature": 4,
-                    "time_signature_confidence": 1
-                },
-                {
-                    "start": 140.97101,
-                    "duration": 40.97663,
-                    "confidence": 0.798,
-                    "loudness": -3.186,
-                    "tempo": 169.792,
-                    "tempo_confidence": 0.554,
-                    "key": 0,
-                    "key_confidence": 0.577,
-                    "mode": 1,
-                    "mode_confidence": 0.565,
-                    "time_signature": 4,
-                    "time_signature_confidence": 1
-                },
-                {
-                    "start": 181.94763,
-                    "duration": 3.32257,
-                    "confidence": 0.872,
-                    "loudness": -22.287,
-                    "tempo": 169.904,
-                    "tempo_confidence": 0.279,
-                    "key": 10,
-                    "key_confidence": 0.05,
-                    "mode": 1,
-                    "mode_confidence": 0.495,
-                    "time_signature": 4,
-                    "time_signature_confidence": 1
+                    "mode_confidence": 0.42,
+                    "time_signature": 1,
+                    "time_signature_confidence": 0.25
                 }
-            ]
-        }
+            ] // nmixx - o.o
+        },
+        audioFeatures: {
+            type: Object,
+            required: false,
+            default: {
+                "danceability": 0.389,
+                "energy": 0.77,
+                "key": 11,
+                "loudness": -3.92,
+                "mode": 0,
+                "speechiness": 0.297,
+                "acousticness": 0.0368,
+                "instrumentalness": 0.00000379,
+                "liveness": 0.0598,
+                "valence": 0.295,
+                "tempo": 200.263,
+                "type": "audio_features",
+                "id": "44zfpg3ndtGESsgpTbWeyE",
+                "uri": "spotify:track:44zfpg3ndtGESsgpTbWeyE",
+                "track_href": "https://api.spotify.com/v1/tracks/44zfpg3ndtGESsgpTbWeyE",
+                "analysis_url": "https://api.spotify.com/v1/audio-analysis/44zfpg3ndtGESsgpTbWeyE",
+                "duration_ms": 172936,
+                "time_signature": 4
+            } // nmixx - o.o
+        } // nmixx - o.o
     },
     setup(props) {
+
+        let detail = 20;
+
         const width = 700;
         const height = 700;
         const radius = 150;
         const svgRef = ref(null);
-        const data = ref(generateRandomData());
         let currentSection = 0;
-        let updateTimeout;
+        let toZero = false;
+        const data = ref(generateData());
+        let updateTimeouts = [];
         let updateInterval;
 
-        function generateRandomData() {
-            return Array.from({ length: 20 }, () => ({ value: Math.random() }));
+        function generateData() {
+            let smoothness = -props.audioAnalysisSections[currentSection].loudness / 6; // higher = smoother (min 0)
+            let data;
+            if (toZero) {
+                data = Array.from({ length: detail }, () => ({ value: 0 }));
+            } else {
+                data = Array.from({ length: detail }, () => ({ value: Math.random() + smoothness }));
+            }
+            toZero = !toZero;
+            return data;
         }
 
         function updateData() {
-            data.value = generateRandomData();
+            data.value = generateData();
         }
 
         function nextSection() {
             console.log('Next section');
             clearInterval(updateInterval);
-            updateInterval = setInterval(updateData, 60000 / props.audioAnalysisSections[currentSection].tempo);
-            updateTimeout = setTimeout(nextSection, props.audioAnalysisSections[currentSection].duration * 1000);
+            updateInterval = setInterval(updateData, 30000 / props.audioAnalysisSections[currentSection].tempo);
             currentSection = (currentSection + 1) % props.audioAnalysisSections.length;
         }
 
@@ -250,10 +299,11 @@ export default {
 
             svg.select('path')
                 .transition()
-                .duration(1000)
-                .ease(d3.easeLinear) // Use linear easing for smooth, constant speed transition
-                .attr('d', pathData)
-                .attr('fill', 'rgba(200, 0, 100, 0.7)');
+                .duration(30000 / props.audioAnalysisSections[currentSection].tempo)
+                //.ease(d3.easeElastic) // easeElastic for a bouncy effect
+                //.ease(d3.easeLinear) // easeLinear for a linear transition
+                .ease(d3.easeSin) // easeSinOut for a smooth transition
+                .attr('d', pathData);
         }
 
         // Watcher for reactive data updates
@@ -261,14 +311,18 @@ export default {
 
 onBeforeUnmount(() => {
     clearInterval(updateInterval);
-    clearTimeout(updateTimeout);
+    updateTimeouts.forEach(timeout => {
+        clearTimeout(timeout);
+    });
 });
 
 onMounted(() => {
     nextTick(() => {
         createVisualizer();
         updateInterval = setInterval(updateData, 60000 / props.audioAnalysisSections[currentSection].tempo);
-        updateTimeout = setTimeout(nextSection, props.audioAnalysisSections[currentSection].duration * 1000);
+        props.audioAnalysisSections.forEach(section => {
+            updateTimeouts.push(setTimeout(nextSection, section.start * 1000));
+        });
     });
 });
 
