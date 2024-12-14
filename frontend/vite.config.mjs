eb84b0ff2e85ae5360c109954d3ad717c9a53681
W00dPeckker <marin.hirschi@stud.hslu.ch>
2024-12-14 23:02:33 +0100

    rollup gsap in vite.config

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -17,5 +17,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['gsap-trial']
+    }
   }
 })
