54cb1883c86a26d7f4408ac93f3c3d296d966d67
W00dPeckker <marin.hirschi@stud.hslu.ch>
2024-12-19 14:34:00 +0100

    fix: lenis SmoothScroll

--- frontend/src/components/CallbackPage.vue
+++ frontend/src/components/CallbackPage.vue
@@ -1,266 +1,289 @@
 <template>
-    <JourneyPage v-if="accessToken && isReady" :player-ready="playerReady" :years="years" :userName="userName"
-        :userImg="userImg">
-    </JourneyPage>
-    <LoadingPage v-else></LoadingPage>
+  <JourneyPage v-if="accessToken && isReady" :player-ready="playerReady" :years="years" :userName="userName"
+               :userImg="userImg">
+  </JourneyPage>
+  <LoadingPage v-else></LoadingPage>
 </template>
 
 <script setup>
 import JourneyPage from './JourneyPage.vue';
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import {ref, onMounted} from 'vue';
+import {useRoute} from 'vue-router';
 import LoadingPage from './LoadingPage.vue';
 
-import { getUserInfo } from '../api/user.js';
-import { getWrappedPlaylists } from '../api/getWrappedPlaylists.js';
-import { getRecentlyPlayed } from '@/api/getRecentlyPlayed.js';
-import { getTopTracks } from '../api/getTopTracks.js';
-import { getTopArtists } from '../api/getTopArtists.js';
-import { getArtist } from '../api/getArtist.js';
+import {getUserInfo} from '../api/user.js';
+import {getWrappedPlaylists} from '../api/getWrappedPlaylists.js';
+import {getRecentlyPlayed} from '@/api/getRecentlyPlayed.js';
+import {getTopTracks} from '../api/getTopTracks.js';
+import {getTopArtists} from '../api/getTopArtists.js';
+import {getArtist} from '../api/getArtist.js';
+import Lenis from "lenis";
+import {ScrollTrigger} from "gsap-trial/ScrollTrigger";
+import gsap from "gsap-trial";
 
 const playerReady = ref(false);
 const accessToken = ref(null);
 const route = useRoute();
 
 const years = ref([
-    {
-        title: 'Now',
-        recentTracks: [],
-    },
-    {
-        title: 'Last 4 Weeks',
-        topTracks: [],
-        topArtists: [],
-        topGenres: []
-    },
-    {
-        title: 'Last 6 Months',
-        topTracks: [],
-        topArtists: [],
-        topGenres: []
-    },
-    {
-        title: 'Last 12 Months',
-        topTracks: [],
-        topArtists: [],
-        topGenres: []
-    }
+  {
+    title: 'Now',
+    recentTracks: [],
+  },
+  {
+    title: 'Last 4 Weeks',
+    topTracks: [],
+    topArtists: [],
+    topGenres: []
+  },
+  {
+    title: 'Last 6 Months',
+    topTracks: [],
+    topArtists: [],
+    topGenres: []
+  },
+  {
+    title: 'Last 12 Months',
+    topTracks: [],
+    topArtists: [],
+    topGenres: []
+  }
 ]);
 const userName = ref('');
 const userImg = ref('');
 const isReady = ref(false);
 
+// Lenis Smooth Scroll
+const lenis = new Lenis({
+  autoRaf: false,
+});
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf);
+
+lenis.on('scroll', ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+
 onMounted(async () => {
-    // const state = route.query.state;
-    // const code = route.query.code;
-    //let currentTime = Math.floor(new Date().getTime() / 1000);
-    //console.log(isNaN(localStorage.getItem('tokenExpirationTime')), currentTime, localStorage.getItem('tokenExpirationTime'));
+  // const state = route.query.state;
+  // const code = route.query.code;
+  //let currentTime = Math.floor(new Date().getTime() / 1000);
+  //console.log(isNaN(localStorage.getItem('tokenExpirationTime')), currentTime, localStorage.getItem('tokenExpirationTime'));
 
-    accessToken.value = route.query.access_token;
-    localStorage.setItem('access_token', route.query.access_token);
+  accessToken.value = route.query.access_token;
+  localStorage.setItem('access_token', route.query.access_token);
 
-    // if (!localStorage.getItem('tokenExpirationTime') || currentTime > localStorage.getItem('tokenExpirationTime')) {
-    //     let access_token_response = await fetch('http://localhost:3000/callback' + "?code=" + code + "&state=" + state,
-    //         { credentials: 'include' }
-    //     )
-    //     let access_token = await access_token_response.json()
-    //     console.log(access_token)
-    //     localStorage.setItem('tokenExpirationTime', currentTime + access_token.expires_in);
-    //     localStorage.setItem('access_token', access_token.access_token);
-    //     localStorage.setItem('refresh_token', access_token.refresh_token);
-    //     accessToken.value = access_token.access_token;
-    //     console.log(accessToken.value);
-    // }
-    // else {
-    //     accessToken.value = localStorage.getItem('access_token');
-    //     console.log("Token still valid: Access Token: ", localStorage.getItem('access_token'), "\n Refresh Token: ", localStorage.getItem('refresh_token'));
-    // }
+  // if (!localStorage.getItem('tokenExpirationTime') || currentTime > localStorage.getItem('tokenExpirationTime')) {
+  //     let access_token_response = await fetch('http://localhost:3000/callback' + "?code=" + code + "&state=" + state,
+  //         { credentials: 'include' }
+  //     )
+  //     let access_token = await access_token_response.json()
+  //     console.log(access_token)
+  //     localStorage.setItem('tokenExpirationTime', currentTime + access_token.expires_in);
+  //     localStorage.setItem('access_token', access_token.access_token);
+  //     localStorage.setItem('refresh_token', access_token.refresh_token);
+  //     accessToken.value = access_token.access_token;
+  //     console.log(accessToken.value);
+  // }
+  // else {
+  //     accessToken.value = localStorage.getItem('access_token');
+  //     console.log("Token still valid: Access Token: ", localStorage.getItem('access_token'), "\n Refresh Token: ", localStorage.getItem('refresh_token'));
+  // }
 
-    const script = document.createElement('script');
-    script.src = "https://sdk.scdn.co/spotify-player.js";
-    script.async = true;
-    document.body.appendChild(script);
+  const script = document.createElement('script');
+  script.src = "https://sdk.scdn.co/spotify-player.js";
+  script.async = true;
+  document.body.appendChild(script);
 
-    window.onSpotifyWebPlaybackSDKReady = () => {
-        const token = localStorage.getItem('access_token');
-        const player = new Spotify.Player({
-            name: 'Spütify',
-            getOAuthToken: cb => { cb(token); },
-            volume: 0.5
-        });
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = localStorage.getItem('access_token');
+    const player = new Spotify.Player({
+      name: 'Spütify',
+      getOAuthToken: cb => {
+        cb(token);
+      },
+      volume: 0.5
+    });
 
-        player.addListener('ready', ({ device_id }) => {
-            playerReady.value = true;
-            localStorage.setItem('device_id', device_id);
-        });
+    player.addListener('ready', ({device_id}) => {
+      playerReady.value = true;
+      localStorage.setItem('device_id', device_id);
+    });
 
-        player.addListener('not_ready', ({ device_id }) => {
-            playerReady.value = false;
-            console.log('Device ID has gone offline', device_id);
-        });
+    player.addListener('not_ready', ({device_id}) => {
+      playerReady.value = false;
+      console.log('Device ID has gone offline', device_id);
+    });
 
-        player.addListener('initialization_error', ({ message }) => {
-            console.error(message);
-        });
+    player.addListener('initialization_error', ({message}) => {
+      console.error(message);
+    });
 
-        player.addListener('authentication_error', ({ message }) => {
-            console.error(message);
-        });
+    player.addListener('authentication_error', ({message}) => {
+      console.error(message);
+    });
 
-        player.addListener('account_error', ({ message }) => {
-            console.error(message);
-        });
+    player.addListener('account_error', ({message}) => {
+      console.error(message);
+    });
 
-        player.connect();
+    player.connect();
 
-    }
+  }
 
-    // Retrieve data from sessionStorage
-    const sessionUserName = sessionStorage.getItem('userName');
-    const sessionUserImg = sessionStorage.getItem('userImg');
-    const sessionYears = JSON.parse(sessionStorage.getItem('years'));
+  // Retrieve data from sessionStorage
+  const sessionUserName = sessionStorage.getItem('userName');
+  const sessionUserImg = sessionStorage.getItem('userImg');
+  const sessionYears = JSON.parse(sessionStorage.getItem('years'));
 
-    if (sessionUserName && sessionUserImg && sessionYears) {
-        userName.value = sessionUserName;
-        userImg.value = sessionUserImg;
-        years.value = sessionYears;
-        await fetchRecentlyPlayed();
-        isReady.value = true;
-        return;
-    }
-
-    async function fetchUserData() {
-        try {
-            const response = await getUserInfo();
-            userName.value = response.display_name;
-            if (response.images.length > 0) {
-                userImg.value = response.images[0].url;
-            } else {
-                userImg.value = "../assets/user.png";
-            }
-        } catch (error) {
-            console.error("Error fetching user info:", error);
-        }
-    }
-
-    function analyseTopItems(topArtists, topTracks, index) {
-        // Fetch top genres for each time period
-        const genreCount = {};
-        topArtists.items.forEach(artist => {
-            if (artist.genres) {
-                artist.genres.forEach(genre => {
-                    genreCount[genre] = (genreCount[genre] || 0) + 1;
-                });
-            }
-        });
-        years.value[index].topGenres = Object.keys(genreCount)
-            .sort((a, b) => genreCount[b] - genreCount[a])
-            .slice(0, 5);
-
-        // Fetch top 3 artists for each genre
-        const genreArtists = {};
-        for (const genre of years.value[index].topGenres) {
-            genreArtists[genre] = [];
-            for (const artist of topArtists.items) {
-                if (artist.genres.includes(genre) && genreArtists[genre].length < 3) {
-                    genreArtists[genre].push(artist);
-                }
-            }
-        }
-        years.value[index].topGenres = years.value[index].topGenres.map(genre => ({
-            name: genre,
-            artists: genreArtists[genre]
-        }));
-
-        // Fetch top tracks for each artist
-        years.value[index].topArtists.forEach(async (_artist, i) => {
-            const artistTopTracks = topTracks.items.filter(track =>
-                track.artists.some(a => a.id === years.value[index].topArtists[i].id)
-            ).slice(0, 3);
-            years.value[index].topArtists[i].tracks = artistTopTracks;
-        });
-    }
-
-    async function fetchTopTracksAndArtists(term, index) {
-        try {
-            const tracksResponse = await getTopTracks(term);
-            years.value[index].topTracks = tracksResponse.items.slice(0, 5);
-
-            const artistsResponse = await getTopArtists(term);
-            years.value[index].topArtists = artistsResponse.items.slice(0, 5);
-
-            analyseTopItems(artistsResponse, tracksResponse, index);
-
-        } catch (error) {
-            console.error(`Error fetching top tracks and artists for ${term}:`, error);
-        }
-    }
-
-    async function fetchWrappedPlaylists() {
-        try {
-            const wrappedPlaylists = await getWrappedPlaylists(localStorage.getItem('device_id'));
-
-            for (const playlist of wrappedPlaylists) {
-                if (playlist.year && playlist.tracks && playlist.tracks.length > 0) {
-
-                    // get top artists for each playlist
-                    const artistCount = {};
-                    playlist.tracks.forEach(track => {
-                        const artistId = track.artists[0].id;
-                        artistCount[artistId] = (artistCount[artistId] || 0) + 1;
-                    });
-
-                    const sortedArtistIds = Object.keys(artistCount).sort((a, b) => artistCount[b] - artistCount[a]);
-
-                    const topArtists = [];
-                    for (const artistId of sortedArtistIds.slice(0, 5)) {
-                        const artist = await getArtist(artistId);
-                        topArtists.push(artist);
-                    }
-
-                    const topTracks = playlist.tracks;
-                    years.value[playlist.index] = Object.assign({}, years.value[playlist.index], {
-                        title: playlist.year,
-                        topTracks: topTracks.slice(0, 5),
-                        topArtists: topArtists.slice(0, 5),
-                        topGenres: []
-                    });
-
-                    analyseTopItems({ items: topArtists }, { items: topTracks }, playlist.index);
-                }
-            }
-        } catch (error) {
-            console.error("Error fetching wrapped playlists:", error);
-        }
-    }
-
-    async function fetchRecentlyPlayed() {
-        try {
-            const response = await getRecentlyPlayed();
-            const recentTracks = response.items.map(item => item.track);
-
-            years.value[0].recentTracks = recentTracks.slice(0, 5);
-        } catch (error) {
-            console.error('Error fetching recently played:', error);
-        }
-    }
-
-    await fetchUserData();
-
+  if (sessionUserName && sessionUserImg && sessionYears) {
+    userName.value = sessionUserName;
+    userImg.value = sessionUserImg;
+    years.value = sessionYears;
     await fetchRecentlyPlayed();
-
-    await fetchTopTracksAndArtists('short_term', 1);
-    await fetchTopTracksAndArtists('medium_term', 2);
-    await fetchTopTracksAndArtists('long_term', 3);
-
-    await fetchWrappedPlaylists();
-
-    // Store data in sessionStorage
-    sessionStorage.setItem('userName', userName.value);
-    sessionStorage.setItem('userImg', userImg.value);
-    sessionStorage.setItem('years', JSON.stringify(years.value));
-
     isReady.value = true;
+    return;
+  }
+
+  async function fetchUserData() {
+    try {
+      const response = await getUserInfo();
+      userName.value = response.display_name;
+      if (response.images.length > 0) {
+        userImg.value = response.images[0].url;
+      } else {
+        userImg.value = "../assets/user.png";
+      }
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+    }
+  }
+
+  function analyseTopItems(topArtists, topTracks, index) {
+    // Fetch top genres for each time period
+    const genreCount = {};
+    topArtists.items.forEach(artist => {
+      if (artist.genres) {
+        artist.genres.forEach(genre => {
+          genreCount[genre] = (genreCount[genre] || 0) + 1;
+        });
+      }
+    });
+    years.value[index].topGenres = Object.keys(genreCount)
+        .sort((a, b) => genreCount[b] - genreCount[a])
+        .slice(0, 5);
+
+    // Fetch top 3 artists for each genre
+    const genreArtists = {};
+    for (const genre of years.value[index].topGenres) {
+      genreArtists[genre] = [];
+      for (const artist of topArtists.items) {
+        if (artist.genres.includes(genre) && genreArtists[genre].length < 3) {
+          genreArtists[genre].push(artist);
+        }
+      }
+    }
+    years.value[index].topGenres = years.value[index].topGenres.map(genre => ({
+      name: genre,
+      artists: genreArtists[genre]
+    }));
+
+    // Fetch top tracks for each artist
+    years.value[index].topArtists.forEach(async (_artist, i) => {
+      const artistTopTracks = topTracks.items.filter(track =>
+          track.artists.some(a => a.id === years.value[index].topArtists[i].id)
+      ).slice(0, 3);
+      years.value[index].topArtists[i].tracks = artistTopTracks;
+    });
+  }
+
+  async function fetchTopTracksAndArtists(term, index) {
+    try {
+      const tracksResponse = await getTopTracks(term);
+      years.value[index].topTracks = tracksResponse.items.slice(0, 5);
+
+      const artistsResponse = await getTopArtists(term);
+      years.value[index].topArtists = artistsResponse.items.slice(0, 5);
+
+      analyseTopItems(artistsResponse, tracksResponse, index);
+
+    } catch (error) {
+      console.error(`Error fetching top tracks and artists for ${term}:`, error);
+    }
+  }
+
+  async function fetchWrappedPlaylists() {
+    try {
+      const wrappedPlaylists = await getWrappedPlaylists(localStorage.getItem('device_id'));
+
+      for (const playlist of wrappedPlaylists) {
+        if (playlist.year && playlist.tracks && playlist.tracks.length > 0) {
+
+          // get top artists for each playlist
+          const artistCount = {};
+          playlist.tracks.forEach(track => {
+            const artistId = track.artists[0].id;
+            artistCount[artistId] = (artistCount[artistId] || 0) + 1;
+          });
+
+          const sortedArtistIds = Object.keys(artistCount).sort((a, b) => artistCount[b] - artistCount[a]);
+
+          const topArtists = [];
+          for (const artistId of sortedArtistIds.slice(0, 5)) {
+            const artist = await getArtist(artistId);
+            topArtists.push(artist);
+          }
+
+          const topTracks = playlist.tracks;
+          years.value[playlist.index] = Object.assign({}, years.value[playlist.index], {
+            title: playlist.year,
+            topTracks: topTracks.slice(0, 5),
+            topArtists: topArtists.slice(0, 5),
+            topGenres: []
+          });
+
+          analyseTopItems({items: topArtists}, {items: topTracks}, playlist.index);
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching wrapped playlists:", error);
+    }
+  }
+
+  async function fetchRecentlyPlayed() {
+    try {
+      const response = await getRecentlyPlayed();
+      const recentTracks = response.items.map(item => item.track);
+
+      years.value[0].recentTracks = recentTracks.slice(0, 5);
+    } catch (error) {
+      console.error('Error fetching recently played:', error);
+    }
+  }
+
+  await fetchUserData();
+
+  await fetchRecentlyPlayed();
+
+  await fetchTopTracksAndArtists('short_term', 1);
+  await fetchTopTracksAndArtists('medium_term', 2);
+  await fetchTopTracksAndArtists('long_term', 3);
+
+  await fetchWrappedPlaylists();
+
+  // Store data in sessionStorage
+  sessionStorage.setItem('userName', userName.value);
+  sessionStorage.setItem('userImg', userImg.value);
+  sessionStorage.setItem('years', JSON.stringify(years.value));
+
+  isReady.value = true;
 });
 </script>
 
--- frontend/src/components/JourneyPage.vue
+++ frontend/src/components/JourneyPage.vue
@@ -1,13 +1,13 @@
 <template>
-  <PopupComponent v-if="showPopup" />
+  <PopupComponent v-if="showPopup"/>
   <div class="bubble" data-speed="0.5">
-    <BubbleComponent :analysis="currentTrack.analysis" :playing="playing" />
+    <BubbleComponent :analysis="currentTrack.analysis" :playing="playing"/>
   </div>
   <div class="timeline">
-    <StickyTimeline :years="years" :active="false" />
+    <StickyTimeline :years="years" :active="false"/>
   </div>
   <div class="mobile-timeline">
-    <MobileTimeline :years="years" :user-name="userName" :user-img="userImg" />
+    <MobileTimeline :years="years" :user-name="userName" :user-img="userImg"/>
   </div>
   <span class="material-symbols-rounded focus-view menu-button" @click="toggleFocusView()">expand_content</span>
   <div class="focus-player">
@@ -15,7 +15,7 @@
       <span class="material-symbols-rounded play-icon">
         {{ playing ? 'pause' : 'play_arrow' }}
       </span>
-      <img class="cover" :src="currentTrack.image" alt="album cover" />
+      <img class="cover" :src="currentTrack.image" alt="album cover"/>
       <p class="song-name">
         {{ currentTrack.name }}<br>
         <span class="song-artist">{{ currentTrack.artist }}</span>
@@ -27,20 +27,20 @@
     <span class="material-symbols-rounded logout-mobile menu-button" @click="logOut()">logout</span>
     <div id="smooth-content">
       <div>
-        <img class="logoJourney" src="../assets/spütify_logo.png" @click="scrollTo" />
+        <img class="logoJourney" src="../assets/spütify_logo.png" @click="scrollTo"/>
       </div>
       <div class="box box-a gradient-black" data-speed="0.5">
-        <WelcomeComponent :user-name="userName" />
+        <WelcomeComponent :user-name="userName"/>
       </div>
       <div class="box box-b gradient-black">
         <YearComponent v-for="(year, index) in years" :key="index" :yearIndex="index" :year="year"
-          :currentTrack="currentTrack" :playing="playing" :playTrack="playTrack" />
+                       :currentTrack="currentTrack" :playing="playing" :playTrack="playTrack"/>
       </div>
       <div class="badge-page">
-        <BadgesComponent :badges="badges" :badgeIndex="years.length" />
+        <BadgesComponent :badges="badges" :badgeIndex="years.length"/>
       </div>
       <div class="share">
-        <ShareComponent :user-name="userName" :years="years" :badges="badges" :shareIndex="years.length + 1" />
+        <ShareComponent :user-name="userName" :years="years" :badges="badges" :shareIndex="years.length + 1"/>
       </div>
       <footer class="footergradient-black">
         <button @click="scrollTo" class="totop">
@@ -53,13 +53,13 @@
 </template>
 
 <script setup>
-import { onMounted, onBeforeUnmount, onUnmounted, ref, nextTick } from 'vue';
+import {onMounted, onBeforeUnmount, onUnmounted, ref, nextTick} from 'vue';
 import gsap from 'gsap-trial';
-import { ScrollTrigger } from 'gsap-trial/ScrollTrigger';
+import {ScrollTrigger} from 'gsap-trial/ScrollTrigger';
 import Lenis from 'lenis';
-import { playback } from '../api/playback.js';
-import { getArtist } from '../api/getArtist.js';
-import { logOut } from '../api/auth.js';
+import {playback} from '../api/playback.js';
+import {getArtist} from '../api/getArtist.js';
+import {logOut} from '../api/auth.js';
 
 import BubbleComponent from './BubbleComponent.vue';
 import StickyTimeline from './StickyTimeline.vue';
@@ -104,6 +104,7 @@ const playing = ref(false);
 let showPopup = handleShowPopup();
 
 let focusView = false;
+
 function toggleFocusView() {
   document.removeEventListener('touchstart', toggleFocusView);
 
@@ -135,7 +136,7 @@ function toggleFocusView() {
           if (!target.closest('.focus-player') && !target.closest('.focus-view')) {
             toggleFocusView();
           }
-        }, { once: true });
+        }, {once: true});
       }, 500);
     } else {
       if (document.exitFullscreen) {
@@ -609,7 +610,6 @@ onMounted(async () => {
 }, main.value);
 
 
-
 onBeforeUnmount(() => {
   clearInterval(updateInterval);
 });
@@ -619,24 +619,8 @@ onUnmounted(() => {
 });
 
 function scrollTo() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
-
-const lenis = new Lenis({
-  autoRaf: false,
-});
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
-
-lenis.on('scroll', ScrollTrigger.update);
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
 
 gsap.ticker.lagSmoothing(0);
 
@@ -653,8 +637,8 @@ window.onload = function () {
         toggleActions: "resume pause reverse pause"
       }
     });
-    panel_tl.from(panel, { opacity: 0, duration: 0.5 });
-    panel_tl.to(panel, { opacity: 0, duration: 0.5 });
+    panel_tl.from(panel, {opacity: 0, duration: 0.5});
+    panel_tl.to(panel, {opacity: 0, duration: 0.5});
   });
 
 };
@@ -1016,7 +1000,6 @@ p {
     top: -30%;
     left: -400%;
   } */
-
   #smooth-wrapper {
     opacity: 1;
     transition: opacity 0.5s;
